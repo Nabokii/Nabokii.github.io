1e491d35685cb7c0ee8d9b1284accc3ba775892e
Nabokii <doodledoothesecond@gmail.com>
2024-11-23 14:55:31 +0000

    new img file and general changes

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//Variables and imported elements from html
 let sidebar = document.getElementById("sidebar")
 let title = document.getElementById("title")
 let titleText = title.textContent
@@ -7,13 +8,18 @@ let subtitleText = subtitle.textContent
 let desiredSubtitleText = "Website by [redacted]"
 let showing = 0
 
+//Function for the fancy little animation of the title and subtitle in the header
 function changeCharacter(old, notOld, DOMelement, speed) {
+    //Temp variable for loop
     let currentIndex = 0
+    //Splits the old text (the 1s and 0s) up into individual letters
     let updatedText = old.split('');
 
     let interval = setInterval(() => {
         if (currentIndex < updatedText.length) {
+            //Changes the specific 1 or 0 that the loop is currently on into the letter that is actually wanted
             updatedText[currentIndex] = notOld[currentIndex];
+            //Changes the element in the html in real time to accomodate the new letter
             DOMelement.textContent = updatedText.join('');
             currentIndex++;
         } else {
@@ -22,6 +28,7 @@ function changeCharacter(old, notOld, DOMelement, speed) {
     }, speed);
 }
 
+//All the things that happen when the website first loads
 document.addEventListener('DOMContentLoaded', function() {
     setTimeout(function() {
         changeCharacter(titleText, desiredTitleText, title, 60)
@@ -34,11 +41,13 @@ document.addEventListener('DOMContentLoaded', function() {
     }, 6000)
 })
 
+//Function to slide the sidebar in when icon is clicked
 function showSidebar() {
     sidebar.classList.remove("slideOut")
     sidebar.classList.add("slideIn")
 }
 
+//Function to slide the sidebar off the page when the close icon is clicked
 function hideSidebar() {
     sidebar.classList.remove("slideIn")
     sidebar.classList.add("slideOut")
