47bc3acc7e4fb5d1004131b7433234e08052e65e
Nabokii <73061576+Nabokii@users.noreply.github.com>
2025-02-25 13:41:24 +0000

    Delete sourceReview.js

--- sourceReview.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('scroll', function() {
-    setTimeout(function() {
-        document.getElementsByClassName("citationDiv").classList.add("fadeIn")
-        document.getElementsByClassName("citationDiv").classList.remove("hidden2")
-    }, 2000)
-})
